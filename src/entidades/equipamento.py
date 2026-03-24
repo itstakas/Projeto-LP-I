35e1def828a2026d9fd2cd4a04d362beb15c913a
Leonardo Takeshi <takeshileonardo.1@gmail.com>
2026-03-24 16:23:33 -0300

    Etapa 1 finalizada

---
+++ src/entidades/equipamento.py
@@ -0,0 +1,37 @@
+equipamentos = []
+
+def get_equipamentos(): return equipamentos
+
+def inserir_equipamento(equipamento): equipamentos.append(equipamento)
+
+def selecionar_equipamentos(prefixo_fabricante=None, peso_máximo_kg=None, crítico=None):
+    filtros = '\nFiltros -- '
+    if prefixo_fabricante is not None: filtros += 'prefixo do fabricante: ' + prefixo_fabricante
+    if peso_máximo_kg is not None: filtros += ' - peso máximo: ' + str(peso_máximo_kg) + ' kg'
+    if crítico is True: filtros += ' - crítico'
+    elif crítico is False: filtros += ' - não crítico'
+    equipamentos_selecionados = []
+    for equipamento in equipamentos:
+        if prefixo_fabricante is not None and not equipamento.fabricante.startswith(prefixo_fabricante): continue
+        if peso_máximo_kg is not None and equipamento.peso_kg > peso_máximo_kg: continue
+        if crítico in (True, False) and equipamento.crítico != crítico: continue
+        equipamentos_selecionados.append(equipamento)
+    return filtros, equipamentos_selecionados
+
+class Equipamento:
+    def __init__(self, nome, fabricante, peso_kg, crítico):
+        self.nome = nome
+        self.fabricante = fabricante
+        self.peso_kg = peso_kg
+        self.crítico = crítico
+
+    def __str__(self):
+        if self.crítico: crítico_str = 'crítico |'
+        else: crítico_str = ''
+        formato = '{} {:<24} {} {:<16} {} {:<9} {} {}'
+        equipamento_formatado = formato.format(
+            '|', self.nome,
+            '|', self.fabricante,
+            '|', f'{self.peso_kg:6.1f}' + ' kg',
+            '|', crítico_str)
+        return equipamento_formatado
---
+++ src/entidades/exploração_espacial.py
@@ -0,0 +1,37 @@
+explorações_espaciais = []
+
+def get_explorações_espaciais(): return explorações_espaciais
+
+def inserir_exploração_espacial(exploração_espacial): explorações_espaciais.append(exploração_espacial)
+
+def selecionar_explorações_espaciais(destino=None, duração_máxima_dias=None, tripulada=None):
+    filtros = '\nFiltros -- '
+    if destino is not None: filtros += 'destino: ' + destino
+    if duração_máxima_dias is not None: filtros += ' - duração máxima: ' + str(duração_máxima_dias) + ' dias'
+    if tripulada is True: filtros += ' - tripulada'
+    elif tripulada is False: filtros += ' - não tripulada'
+    explorações_selecionadas = []
+    for exploração_espacial in explorações_espaciais:
+        if destino is not None and exploração_espacial.destino != destino: continue
+        if duração_máxima_dias is not None and exploração_espacial.duração_dias > duração_máxima_dias: continue
+        if tripulada in (True, False) and exploração_espacial.tripulada != tripulada: continue
+        explorações_selecionadas.append(exploração_espacial)
+    return filtros, explorações_selecionadas
+
+class ExploraçãoEspacial:
+    def __init__(self, nome, destino, duração_dias, tripulada):
+        self.nome = nome
+        self.destino = destino if destino in ('lua', 'marte', 'órbita_terrestre', 'asteroide', 'júpiter') else 'indefinido'
+        self.duração_dias = duração_dias
+        self.tripulada = tripulada
+
+    def __str__(self):
+        if self.tripulada: tripulada_str = 'tripulada |'
+        else: tripulada_str = ''
+        formato = '{} {:<22} {} {:<17} {} {:<9} {} {}'
+        exploração_formatada = formato.format(
+            '|', self.nome,
+            '|', self.destino,
+            '|', f'{self.duração_dias:4d}' + ' dias',
+            '|', tripulada_str)
+        return exploração_formatada
